0c69eca39e2cb311aee4a7e434ccc5175d06d1b5
David Stockhouse <dstockhouse@gmail.com>
2020-01-26 16:36:39 -0700

    Removed old vn200 code to comply with non-git-friendly changes made to master

--- navigation/script/puttyLog.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p $HOME/icarus-dronenet/log/putty
-logfilename="VN200_$(date +%Y.%m.%d_%H-%M-%S).bin"
-plink -serial /dev/ttyUSB0 -sercfg $1 | tee $HOME/icarus-dronenet/log/putty/$logfilename
-
--- navigation/script/trim_samples.sh
+++
@@ -1,1 +0,0 @@
-rm -f $(ls -lh | sort -h -k 5 | awk '{print $9}' | head -100)
