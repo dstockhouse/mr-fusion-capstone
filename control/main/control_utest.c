286c7969367164a987568fa8c209cbe1c35b066a
David Stockhouse <dstockhouse@gmail.com>
2020-01-26 21:34:09 -0700

    Makefile improved towards generalized build system

--- control/main/control_utest.c
+++ control/main/control_utest.c
@@ -8,7 +8,7 @@ BeforeEach(Strlen) {}
 AfterEach(Strlen) {}
 
 Ensure(Strlen, returns_five_for_hello) {
-    assert_that(strlen("hello"), is_equal_to(5));
+	assert_that(strlen("hello"), is_equal_to(5));
 }
 
 int main(int argc, char** argv) {
