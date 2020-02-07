21dcecb686800f48e55e0ea2189ee16111277b8c
seanybaggins <seli2854@colorado.edu>
2020-02-06 20:35:27 -0700

    added exercises

--- navigation/main/navigation_test.c
+++ navigation/main/navigation_test.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "navigation_run.h"
 
+/*
 Describe(Strlen);
 BeforeEach(Strlen) {}
 AfterEach(Strlen) {}
@@ -11,17 +12,30 @@ AfterEach(Strlen) {}
 Ensure(Strlen, returns_five_for_hello) {
 	assert_that(strlen("hello"), is_equal_to(5));
 }
+*/
 
+/* Exercise 1
+ * Given a function that takes an integer and shifts it's bit's to the right,
+ * write a unit tests that checks is the function is working properly.
+ * 
 Describe(BitShiftRight);
-BeforeEach(BitShiftRight) {
-	int number = 5;
-}
+BeforeEach(BitShiftRight) {}
 AfterEach(BitShiftRight) {}
 
 Ensure(BitShiftRight, shiftsBitRight) {
+	int number = 5;
 	int result = bit_shift_right(number);
 	assert_that(result, is_equal_to(2));
 }
+*/
+
+/* Exercise 2
+ * Write a unit tests a function that takes an integer and returns the integer
+ * with the bits shifted to the left.
+ * 
+ * After the unit test has been written, write the function that performs this operation.
+
+*/
 
 int main(int argc, char** argv) {
 
