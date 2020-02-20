a2878f0bd544914343af3627d951173de3fbbaac
rockwec1 <44419777+rockwec1@users.noreply.github.com>
2020-02-20 15:22:25 -0700

    Update ControlSim.m

--- control/ControlSim.m
+++ control/ControlSim.m
@@ -3,11 +3,11 @@ function [theta_L, theta_R] = ControlSim(omega)
 % Name: ControlSim
 % Desc: Simulates the robot's control scheme. Reads in a rate of rotation
 % and produces angular velocities for the left and right motors
-% Inputs: omega - rate of rotation for the robot
-% Outputs: theta_L - angular velocity of left wheel
-%          theta_R - angular velocity of right wheel
+% Inputs: omega - rate of rotation for the robot in rad/sec
+% Outputs: theta_L - angular velocity of left wheel in rad/sec
+%          theta_R - angular velocity of right wheel in rad/sec
 % Author: Connor Rockwell, Joy Fucella, Duncan Patel
-% Last Modified: 2/17/2020
+% Last Modified: 2/20/2020
 %--------------------------------------------------------------------------
 
 %--------------------------------------------------------------------------
@@ -16,7 +16,7 @@ function [theta_L, theta_R] = ControlSim(omega)
 r = .5524;          % radius of the wheels
 R = .1524;          % radius of the axle
 
-Vsys = 0.5;         % constant linear velocity of the system in m/s
+Vsys = 1;           % constant linear velocity of the system in m/s
 
 %--------------------------------------------------------------------------
 % Define controller
@@ -30,7 +30,7 @@ D = Vsys/omega;     % distance between center/curvature and center/axle
 vL = omega*(D + R); % linear velocity of left wheel
 vR = omega*(D - R); % linear velocity of right wheel
 
-theta_L = vL*r;     % angular velocity of left wheel
-theta_R = vR*r;     % angular velocity of right wheel
+theta_L = vL/r*3;   % angular velocity of left wheel
+theta_R = vR/r*3;   % angular velocity of right wheel
                                  
 end % End of function
