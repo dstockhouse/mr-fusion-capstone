b35e8f6ed8450edd5a0cb93a4da28d1e8f8fadb8
Stockhouse <STOCKHOD@my.erau.edu>
2020-02-19 17:31:50 -0700

    Work in progress fixing lie algebra stuff

--- sensors/tof/optical_flow/trans2kai.m
+++ sensors/tof/optical_flow/trans2kai.m
@@ -10,14 +10,32 @@ function kai = trans2kai(trans)
 %     Velocity vector of the form [vx vy vz wx wy wz]' where v is the
 %     linear velocity and w is the angular velocity
 
+% Get linear and rotational parts from transformation matrix
+rot_mat = trans(1:3, 1:3);
+lin_trans = reshape(trans(1:3, 4), 3, 1);
+
 % Use logarithm in SE(3) to turn rigid transformation matrix into twist
 % vector
-rot_mat = trans(1:3, 1:3);
-[k, theta] = dcm2k_theta(rot_mat);
-w = k*theta;
-w_cross = vec2ss(w);
+% Adapted from http://ethaneade.com/lie.pdf and http://karnikram.info/blog/lie/
 
-lin_trans = reshape(trans(1:3, 4), 3, 1);
+theta = acos((trace(R) - 1)/2);
+if abs(theta) < 0.0000001
+    w = zeros(3,1);
+    v = lin_trans;
+else
+    w_pre = [rot_mat(3,2) - rot_mat(2,3);
+             rot_mat(1,3) - rot_mat(3,1);
+             rot_mat(2,1) - rot_mat(1,2)];
+    w = theta / (2*sin(theta)) * w_pre;
+    w_cross = vec2ss(w);
+    V_inv = (eye(3) - w_cross/2 + (w_cross^2 * 
+    v = 
+end
+
+% [k, theta] = dcm2k_theta(rot_mat);
+% w = k*theta;
+% w_cross = vec2ss(w);
+
 if abs(theta) < .00001
     % Approximate to avoid singularities if theta very small
     A = 1;
