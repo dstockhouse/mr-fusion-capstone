fe7ee93d584cd6985135859641c9f8e1023db9b5
rockwec1 <44419777+rockwec1@users.noreply.github.com>
2020-02-21 13:16:50 -0700

    Update ControlSim.m

--- control/ControlSim.m
+++ control/ControlSim.m
@@ -1,13 +1,13 @@
-function [theta_L, theta_R] = ControlSim(omega)
+function [theta_L, theta_R] = ControlSim(delta_heading)
 %--------------------------------------------------------------------------
 % Name: ControlSim
 % Desc: Simulates the robot's control scheme. Reads in a rate of rotation
 % and produces angular velocities for the left and right motors
-% Inputs: omega - rate of rotation for the robot in rad/sec
+% Inputs: delta_heading - change in heading in radians
 % Outputs: theta_L - angular velocity of left wheel in rad/sec
 %          theta_R - angular velocity of right wheel in rad/sec
 % Author: Connor Rockwell, Joy Fucella, Duncan Patel
-% Last Modified: 2/20/2020
+% Last Modified: 2/21/2020
 %--------------------------------------------------------------------------
 
 %--------------------------------------------------------------------------
@@ -18,6 +18,8 @@ R = .1524;          % radius of the axle
 
 Vsys = 1;           % constant linear velocity of the system in m/s
 
+Kp = 3;             % controller constant
+
 %--------------------------------------------------------------------------
 % Define controller
 %--------------------------------------------------------------------------
@@ -25,12 +27,14 @@ Vsys = 1;           % constant linear velocity of the system in m/s
 % Used to determine the center of curvature based on a desired heading
 % ICC = [Xpos - R*sin(heading),Ypos + R*cos(heading)];
 
-D = Vsys/omega;     % distance between center/curvature and center/axle
+% D = Vsys/omega;      % distance between center/curvature and center/axle
 
-vL = omega*(D + R); % linear velocity of left wheel
-vR = omega*(D - R); % linear velocity of right wheel
+omega = delta_heading*Kp; % rate of angular change of robot
 
-theta_L = vL/r*3;   % angular velocity of left wheel
-theta_R = vR/r*3;   % angular velocity of right wheel
+vL = Vsys - R*omega;      % linear velocity of left wheel
+vR = Vsys + R*omega;      % linear velocity of right wheel
+
+theta_L = vL/r;           % angular velocity of left wheel
+theta_R = vR/r;           % angular velocity of right wheel
                                  
 end % End of function
