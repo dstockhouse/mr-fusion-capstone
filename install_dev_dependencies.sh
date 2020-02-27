42a68c040058755a5cb622de0b10d21eacaac2cb
seanybaggins <seli2854@colorado.edu>
2020-02-27 14:50:26 -0700

    updated dev dependancies for remote sshing into our machines

--- install_dev_dependencies.sh
+++ install_dev_dependencies.sh
@@ -3,3 +3,7 @@
 # C unit testing library (cgreen)
 wget https://github.com/cgreen-devs/cgreen/releases/download/1.2.0/cgreen_1.2.0_amd64.deb -P ~/Downloads
 sudo dpkg -i ~/Downloads/cgreen_1.2.0_amd64.deb
+
+# For determining the ip address of our R Pi's using their MAC address
+sudo apt install arp-scan
+
