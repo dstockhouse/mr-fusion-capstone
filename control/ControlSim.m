9cbe86cff3f640881dfcdeeb04539f98c144bdcc
rockwec1 <44419777+rockwec1@users.noreply.github.com>
2020-03-01 17:12:27 -0700

    Update ControlSim.m

--- control/ControlSim.m
+++ control/ControlSim.m
@@ -1,13 +1,15 @@
-function [theta_L, theta_R] = ControlSim(delta_heading)
+function [theta_L, theta_R] = ControlSim(delta_heading, speed)
 %--------------------------------------------------------------------------
 % Name: ControlSim
 % Desc: Simulates the robot's control scheme. Reads in a desired heading
 %       change and produces angular velocities for the motors.
 % Inputs: delta_heading - change in heading in radians
+%         speed - a command to determine whether or not the robot should be
+%                 moving while turning
 % Outputs: theta_L - angular velocity of left wheel in rad/sec
 %          theta_R - angular velocity of right wheel in rad/sec
 % Author: Connor Rockwell, Joy Fucella, Duncan Patel
-% Last Modified: 2/22/2020
+% Last Modified: 3/01/2020
 %--------------------------------------------------------------------------
 
 %--------------------------------------------------------------------------
@@ -16,7 +18,7 @@ function [theta_L, theta_R] = ControlSim(delta_heading)
 r = .5524;          % radius of the wheels
 R = .1524;          % radius of the axle
 
-Vsys = 1;           % constant linear velocity of the system in m/s
+Vsys = speed;       % constant linear velocity of the system in m/s
 
 Kp = 3;             % controller constant
 
@@ -24,17 +26,19 @@ Kp = 3;             % controller constant
 % Define controller
 %--------------------------------------------------------------------------
 
-% Used to determine the center of curvature based on a desired heading
-% ICC = [Xpos - R*sin(heading),Ypos + R*cos(heading)];
+if Vsys == 0                  % Set wheel velocities manually if a system 
+    theta_L = 1;             % velocity of 0 is received
+    theta_R = -1;
+    
+else
+    omega = delta_heading*Kp; % rate of angular change of robot
 
-% D = Vsys/omega;      % distance between center/curvature and center/axle
+    vL = Vsys - R*omega;      % linear velocity of left wheel
+    vR = Vsys + R*omega;      % linear velocity of right wheel
 
-omega = delta_heading*Kp; % rate of angular change of robot
+    theta_L = vL/r;           % angular velocity of left wheel
+    theta_R = vR/r;           % angular velocity of right wheel
+    
+end
 
-vL = Vsys - R*omega;      % linear velocity of left wheel
-vR = Vsys + R*omega;      % linear velocity of right wheel
-
-theta_L = vL/r;           % angular velocity of left wheel
-theta_R = vR/r;           % angular velocity of right wheel
-                                 
 end % End of function
