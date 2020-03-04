b5ace76dea24650c772e03638bb38f3970f4f120
rockwec1 <44419777+rockwec1@users.noreply.github.com>
2020-03-04 14:59:06 -0700

    Added skeleton comments to file

--- control/src/control_run.c
+++ control/src/control_run.c
@@ -2,7 +2,13 @@
 
 int control_run(int arg) {
 
-	// Add control operation here
+	// First function call will initialize interface with hardware
+
+	// Second function call will read data into controller
+
+	// Third function call will feed data from controller into kinematics
+
+	// Fourth function call will probably do something significant
 
 	return 0;
 }
