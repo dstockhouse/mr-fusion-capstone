e9669c34b8e7856c6162c701cb9349edca1b4392
seanybaggins <seli2854@colorado.edu>
2020-03-06 14:42:32 -0700

    mocking failure continues

--- guidance/rust_code/src/network/mod.rs
+++ guidance/rust_code/src/network/mod.rs
@@ -1,9 +1,26 @@
-use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
-
+use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs};
+use mockall::*;
 use cfg_if::cfg_if;
+use std::io::Result;
 
 const GUIDANCE_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 1);
 const CONTROL_PORT: u16 = 31401;
+
+cfg_if! {
+    if #[cfg(test)] {
+
+        mock! {
+            pub TcpListener {
+                fn bind<A: ToSocketAddrs + 'static>(addr: A) -> Result<TcpListener>;
+            }
+        }
+        
+        use MockTcpListener as TcpListener;
+    }
+    else {
+        use std::net::TcpListener;
+    }
+}
 
 // This function is not finished yet
 pub(self) fn establish_connection(address: Ipv4Addr, port: u16) {
--- guidance/rust_code/src/network/tests.rs
+++ guidance/rust_code/src/network/tests.rs
@@ -2,17 +2,8 @@ use super::*;
 use std::io::Result;
 use std::net::ToSocketAddrs;
 
-use mockall::*;
-
-mock! {
-    pub TcpListener {
-        pub fn bind<A: ToSocketAddrs + 'static>(addr: A) -> Result<TcpListener>;
-    }
-}
-
 #[test]
-fn estblish_connection_unable_to_bind() {
+fn establish_connection() {
     let mock_tcp_listener = MockTcpListener::new();
-
-    mock_tcp_listener.expect_bind()
+    mock_tcp_listener.expect_bind();
 }
