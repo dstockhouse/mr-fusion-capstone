d62f24763521b8785039760a254e5fa047c154f5
seanybaggins <seli2854@colorado.edu>
2020-03-06 14:10:24 -0700

    failed mocking attempt 2

--- guidance/rust_code/src/network/tests.rs
+++ guidance/rust_code/src/network/tests.rs
@@ -1,2 +1,18 @@
 use super::*;
+use std::io::Result;
+use std::net::ToSocketAddrs;
 
+use mockall::*;
+
+mock! {
+    pub TcpListener {
+        pub fn bind<A: ToSocketAddrs + 'static>(addr: A) -> Result<TcpListener>;
+    }
+}
+
+#[test]
+fn estblish_connection_unable_to_bind() {
+    let mock_tcp_listener = MockTcpListener::new();
+
+    mock_tcp_listener.expect_bind()
+}
