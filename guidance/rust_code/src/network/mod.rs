41c25df9dbfaa77d011333fb55c0cb39a834eb33
seanybaggins <seli2854@colorado.edu>
2020-03-06 20:12:26 -0700

    semi working tests for networking

--- guidance/rust_code/src/network/mod.rs
+++ guidance/rust_code/src/network/mod.rs
@@ -1,31 +1,56 @@
-use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs};
-use mockall::*;
+use std::net::{Ipv4Addr, SocketAddrV4, SocketAddr};
+use std::io;
 use cfg_if::cfg_if;
-use std::io::Result;
-
-const GUIDANCE_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 1);
-const CONTROL_PORT: u16 = 31401;
 
 cfg_if! {
+
     if #[cfg(test)] {
+
+        use mockall::*;
+        use std::io::Result;
+        use std::net::ToSocketAddrs;
+
+        mock! {
+            pub TcpStream {}
+        }
+
+        use MockTcpStream as TcpStream;
 
         mock! {
             pub TcpListener {
                 fn bind<A: ToSocketAddrs + 'static>(addr: A) -> Result<TcpListener>;
+                fn set_nonblocking(&self, nonblocking: bool) -> Result<()>;
+                fn accept(&self) -> Result<(TcpStream, SocketAddr)>;
             }
         }
-        
+
         use MockTcpListener as TcpListener;
     }
+
     else {
-        use std::net::TcpListener;
+        use std::net::{TcpListener, TcpStream};
     }
 }
 
+const GUIDANCE_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 1);
+const CONTROL_PORT: u16 = 31401;
+
 // This function is not finished yet
-pub(self) fn establish_connection(address: Ipv4Addr, port: u16) {
+pub(self) fn establish_connection(address: Ipv4Addr, port: u16) -> io::Result<(TcpStream, SocketAddr)> {
     let socket = SocketAddrV4::new(address, port);
-    let tcp_listener = TcpListener::bind(socket); // invocation of the method I am trying to mock
+    let tcp_listener = match TcpListener::bind(socket) {
+        Ok(tcp_listener) => tcp_listener,
+        Err(error) => panic!("unable to bind {:?}", error)
+        // TODO: Send message to UI
+    };
+
+    match tcp_listener.set_nonblocking(true) {
+        Ok(_) => (),
+        Err(error) => unimplemented!() // TODO: Send message to UI we were not able 
+    }
+
+    tcp_listener.accept()
+    
 }
 
 #[cfg(test)]
--- guidance/rust_code/src/network/tests.rs
+++ guidance/rust_code/src/network/tests.rs
@@ -1,9 +1,40 @@
 use super::*;
-use std::io::Result;
-use std::net::ToSocketAddrs;
+use std::net::IpAddr;
 
 #[test]
-fn establish_connection() {
-    let mock_tcp_listener = MockTcpListener::new();
-    mock_tcp_listener.expect_bind();
+#[should_panic]
+fn establish_connection_unable_to_bind() {
+
+    let mock_bind = MockTcpListener::bind_context();
+    mock_bind.expect()
+        // The error returned is arbitrary. Just want to make sure that if there is an error,
+        // we notify the UI and Panic
+        .returning(|_: SocketAddrV4| Err(io::Error::last_os_error())); 
+
+    let _ = establish_connection(GUIDANCE_IP, CONTROL_PORT);
+}
+
+#[test]
+fn establish_connection_able_to_bind_non_blocking() {
+    
+    let mock_bind = MockTcpListener::bind_context();
+    mock_bind.expect()
+        // Please ignore the type parameter to the input of returning since it is unused to set the
+        // return.
+        .returning(|_: SocketAddrV4| {
+
+            let mut mock_tcp_listener = MockTcpListener::new();
+            mock_tcp_listener.expect_set_nonblocking()
+                .returning(|_| Ok(()));
+
+            mock_tcp_listener.expect_accept()
+                .returning(|| Ok((
+                    MockTcpStream::new(), 
+                    SocketAddr::new(IpAddr::V4(GUIDANCE_IP), CONTROL_PORT))
+                )); // Return stating non-blocking was successful
+
+            Ok(mock_tcp_listener)
+        });
+
+    assert!(establish_connection(GUIDANCE_IP, CONTROL_PORT).is_ok());
 }
